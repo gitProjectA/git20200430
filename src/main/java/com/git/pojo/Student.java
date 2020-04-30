7da4c47ef9ec8c1c48a371cd7301efcbadb54a37
gitProjectB <2081262367@qq.com>
2020-04-30 17:33:08 +0800

    解决冲突

--- src/main/java/com/git/pojo/Student.java
+++ src/main/java/com/git/pojo/Student.java
@@ -2,13 +2,9 @@
 package com.git.pojo;
 
 public class Student {
-<<<<<<< HEAD
    private String name;//名字
    private Integer age;//年龄
-=======
-   private String name;//name
-   private Integer age;//age
->>>>>>> branch 'master' of https://github.com/gitProjectA/git20200430.git
+
 public Student() {
 	super();
 	// TODO Auto-generated constructor stub
