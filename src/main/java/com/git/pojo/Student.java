e687ec518e86cdef3fe58dd03c8c8b3c55ccffed
wxl_123 <wxl@qq.com>
2020-04-30 15:40:31 +0800

    创建项目，添加Student类

---
+++ src/main/java/com/git/pojo/Student.java
@@ -0,0 +1,23 @@
+package com.git.pojo;
+
+public class Student {
+   private String name;
+   private Integer age;
+public Student() {
+	super();
+	// TODO Auto-generated constructor stub
+}
+public String getName() {
+	return name;
+}
+public void setName(String name) {
+	this.name = name;
+}
+public Integer getAge() {
+	return age;
+}
+public void setAge(Integer age) {
+	this.age = age;
+}
+   
+}
